8127007d2c94de0183f461db9b6b393bb296c40d
Karl Oakes <karl.oakes@cef.co.uk>
2016-02-24 08:43:53 +0000

    Add UNSPSC classification node to ItemDetail if the data attribute is passed. Version bump

--- lib/cxml/item_detail.rb
+++ lib/cxml/item_detail.rb
@@ -9,11 +9,13 @@ module CXML
     attr_accessor :description
     attr_accessor :unit_of_measure
     attr_accessor :unit_price
+    attr_accessor :unspsc
 
     def initialize(data={})
       if data.kind_of?(Hash) && !data.empty?
         @description = data['Description']
         @unit_of_measure = data['UnitOfMeasure']
+        @unspsc = data['ClassificationUnspsc']
         @unit_price = CXML::Money.new(data['UnitPrice']['Money'])
       end
     end
@@ -23,6 +25,7 @@ module CXML
         node.Description(description)
         node.UnitOfMeasure(unit_of_measure)
         node.UnitPrice{unit_price.render(node)}
+        node.Classification(unspsc, {'domain' => 'UNSPSC'}) unless unspsc.blank?
       end
     end
 
