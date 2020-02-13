75a56268f66e4c015752567359a3f195da5d38a1
Eleni Chappen <eleni@officeluv.com>
2020-02-13 17:27:59 -0600

    new:usr: add buyer_cookie and user_email attributes

--- lib/cxml/punch_out_setup_request.rb
+++ lib/cxml/punch_out_setup_request.rb
@@ -1,8 +1,9 @@
 module CXML
   class PunchOutSetupRequest
-
     attr_accessor :browser_form_post_url
     attr_accessor :supplier_setup_url
+    attr_accessor :buyer_cookie
+    attr_accessor :user_email
 
     def initialize(data={})
       if data.kind_of?(Hash) && !data.empty?
@@ -10,6 +11,9 @@ module CXML
         @browser_form_post_url = browser_form_post['URL']
         supplier_setup = data['SupplierSetup'] || {}
         @supplier_setup_url = supplier_setup['URL']
+        @buyer_cookie = data['BuyerCookie'] || {}
+        user_email_obj = (data['Extrinsic'] || []).find { |item| item['name'] == 'UserEmail' }
+        @user_email = user_email_obj ? user_email_obj['content'] : nil
       end
     end
 
