34b0b6735c0a7bf7a13a0e60f3760e8d00d44d64
andjosh <email@andjosh.com>
2020-04-13 14:23:11 -0500

    chg:dev: Allow multiple InvoiceDetailItems on InvoiceDetailOrder

--- lib/cxml/invoice_detail_order.rb
+++ lib/cxml/invoice_detail_order.rb
@@ -4,7 +4,21 @@ module CXML
   class InvoiceDetailOrder < DocumentNode
     accessible_nodes %i[
       invoice_detail_order_info
-      invoice_detail_item
+      invoice_detail_items
     ]
+
+    def initialize_invoice_detail_item(value)
+      value = [value] unless value.is_a?(Array)
+      @invoice_detail_items = value.map do |item|
+        InvoiceDetailItem.new(item)
+      end
+    end
+
+    def initialize_invoice_detail_items(value)
+      value = [value] unless value.is_a?(Array)
+      @invoice_detail_items = value.map do |item|
+        InvoiceDetailItem.new(item)
+      end
+    end
   end
 end
